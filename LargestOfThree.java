68df9d75573912baf3d34eccf50a4df8ffc08820
pavnisharmaps <pavnisharmaps@gmail.com>
2026-08-21 20:05:42 +0530

    Largest of three numbers program

---
+++ LargestOfThree.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class LargestOfThree {
+    public static void main (String [] args)
+    {
+        Scanner sc = new Scanner (System .in);
+        System.out.println("Enter three numbers");
+        int num1= sc.nextInt();
+        int num2= sc.nextInt();
+        int num3= sc.nextInt();
+        if (num1>num2&&num1>num3)
+        {
+            System.out.println(num1+" is the largest number");
+        }
+        else if (num2>num1&&num2>num3)
+        {
+            System.out.println(num2+" is the largest number");
+        }
+        else
+        {
+            System.out.println(num3+" is the largest number");
+        }
+    }
+}
